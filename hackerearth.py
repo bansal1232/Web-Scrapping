f4870a9a976afad2f26a74e8d861d13904b2462b
bansal1232 <shubhambansal1232@gmail.com>
2017-10-24 00:53:53 +0530

    Add files via upload

---
+++ hackerearth.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+handle='bansal1232'
+url='https://www.hackerearth.com/@'+handle
+r=requests.get(url)
+#print(r.text)
+
+soup=BeautifulSoup(r.text,'lxml')
+soup.findAll
+number=soup.findAll( 'span',class_='track-following-num')
+print('Hackerearth Rating =',number[1].text)
