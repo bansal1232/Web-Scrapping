ab6e174c9eb2a66c35701b036c4451fd7b2160c7
bansal1232 <Shubham bDansal>
2018-02-08 19:31:12 +0530

    New

--- Codechef_solution/conf.py
+++
@@ -1,8 +0,0 @@
-user_key = 'name'
-handle = '' #Write down your handle name 
-
-pass_key = 'pass'
-pass_val = '' # Write down your password
-
-form_key= 'form_id'
-form_val = 'new_login_form'
--- Codechef_solution/crawl.py
+++
@@ -1,9 +0,0 @@
-import code, codechef_solution
-import requests, conf
-from models import URL
-
-with requests.Session() as s:
-	code.codechef_login(s, URL.BASE)
-	code.get_rating(s, conf.handle)
-	codechef_solution.codechef_download(s, conf.handle)
-	code.codechef_logout(s, URL.BASE + URL.LOGOUT)
